2a651e195ef3672f37d2bbcbf53236593c35f2ec
thiagojc89 <37465644+thiagojc89@users.noreply.github.com>
2018-07-05 19:26:25 -0500

    prefiro python v1.0

--- app/src/main/java/com/thiagojc89/franfodao/MainActivity.kt
+++ app/src/main/java/com/thiagojc89/franfodao/MainActivity.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView_main.layoutManager = LinearLayoutManager(this)
-        recyclerView_main.adapter = MainAdapter()
+//        recyclerView_main.adapter = MainAdapter()
         fetchGetConfiguration()
         fetchJson()
         display("Thiago Cavalcante")
@@ -28,9 +29,21 @@ class MainActivity : AppCompatActivity() {
 
     fun fetchGetConfiguration(){
         val urlConfig = "https://api.themoviedb.org/3/configuration?api_key=3bd23754eab8b7f3bfb1f5a8955e3fe4"
+        val requestConfig = Request.Builder().url(urlConfig).build()
+        val client = OkHttpClient()
+        client.newCall(requestConfig).enqueue(object: Callback{
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                val gson = GsonBuilder().create()
+                val apiConfig = gson.fromJson(body, ApiConfig::class .java)
+                
+            }
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed request Call")
+            }
 
+        })
     }
-
     fun fetchJson(){
         val urlApiRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=$apiKey"
         val requestTopRated = Request.Builder().url(urlApiRated).build()
@@ -54,8 +67,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+class ApiConfig(val images :List<ImgConfig>)
+class ImgConfig(val base_url: String, val secure_base_url: String, val backdrop_sizes: List<BackDrop>)//,
+//                val poster_sizes: List<Poster>)
+class BackDrop(var x: MutableList<String>)
+//class Poster(val y: List<String>)
 
-class Homefeed(val results :List<movieList>)
-class movieList(val id: Int, val video: Boolean,
+class Homefeed(val results :List<MovieList>)
+class MovieList(val id: Int, val video: Boolean,
                 val title: String, val poster_path: String,
                 val backdrop_path: String, val release_date: Date)
