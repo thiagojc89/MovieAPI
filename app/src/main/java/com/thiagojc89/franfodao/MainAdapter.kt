a55d5b5a3f1df0f63a92c0193e8975b3e7a96578
thiagojc89 <37465644+thiagojc89@users.noreply.github.com>
2018-07-30 18:01:11 -0500

    Picasso

--- app/src/main/java/com/thiagojc89/franfodao/MainAdapter.kt
+++ app/src/main/java/com/thiagojc89/franfodao/MainAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.movie_content_row.view.*
 
-class MainAdapter(val homeFeed: Homefeed): RecyclerView.Adapter<CustomViewHolder>(){
+class MainAdapter(private val homeFeed: Homefeed): RecyclerView.Adapter<CustomViewHolder>(){
 
     override fun getItemCount(): Int {
         return homeFeed.results.count()
@@ -15,22 +15,22 @@ class MainAdapter(val homeFeed: Homefeed): RecyclerView.Adapter<CustomViewHolder
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        val layoutInflater = LayoutInflater.from(parent?.context)
+        val layoutInflater = LayoutInflater.from(parent.context)
         val cellForRow = layoutInflater.inflate(R.layout.movie_content_row,parent, false)
         return CustomViewHolder(cellForRow)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 
-        val results = homeFeed.results.get(position)
-        holder?.view?.textView_video_title?.text = results.title
+        val results = homeFeed.results[position]
+        holder.view.textView_video_title?.text = results.title
         val urlImageOriginal = "https://image.tmdb.org/t/p/w500/"
-        val imagem = holder?.view.imageViewPoster
-        Picasso.with(holder.view.context)
+        val imagem = holder.view.imageViewPoster
+        Picasso.get()
+//        Picasso.with(holder.view.context)
                 .load(urlImageOriginal+results.poster_path)
                 .into(imagem)
     }
 }
-
-class CustomViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+class CustomViewHolder(val view : View) : RecyclerView.ViewHolder(view){
 }
