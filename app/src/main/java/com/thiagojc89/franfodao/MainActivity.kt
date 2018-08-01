a71efc87c32b13686da3ea0eedcf40603b15ffbd
thiagojc89 <37465644+thiagojc89@users.noreply.github.com>
2018-07-31 21:13:04 -0500

    bye

--- app/src/main/java/com/thiagojc89/franfodao/MainActivity.kt
+++ app/src/main/java/com/thiagojc89/franfodao/MainActivity.kt
@@ -2,32 +2,28 @@ package com.thiagojc89.franfodao
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.GridLayoutManager
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
+
 class MainActivity : AppCompatActivity() {
 
-    private fun display(string: String){
-        println(">>>>> $string")
-    }
     private val apiKey = "3bd23754eab8b7f3bfb1f5a8955e3fe4"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recyclerView_main.layoutManager = LinearLayoutManager(this)
-//        recyclerView_main.adapter = MainAdapter()
-//        fetchGetConfiguration()
+        recyclerView_main.layoutManager = GridLayoutManager(this,2)
         fetchJson()
-        display("Thiago Cavalcante")
     }
 
     private fun fetchJson(){
         val urlApiRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=$apiKey"
         val requestTopRated = Request.Builder().url(urlApiRated).build()
         val client = OkHttpClient()
+
         client.newCall(requestTopRated).enqueue(object: Callback{
             override fun onResponse(call: Call?, response: Response?) {
                 val body = response?.body()?.string()
@@ -35,6 +31,7 @@ class MainActivity : AppCompatActivity() {
                 val homeFeed = gson.fromJson(body, Homefeed::class .java)
 
                 runOnUiThread {
+
                     recyclerView_main.adapter = MainAdapter(homeFeed)
                 }
 
